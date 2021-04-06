fabf7d21de137b881c71d189c8fc2492e3cb973f
bhargavinnovius <59646575+bhargavinnovius@users.noreply.github.com>
2021-04-06 19:20:50 +0530

    06-04-2021 final

--- app/Http/Controllers/API/TwilioController.php
+++ app/Http/Controllers/API/TwilioController.php
@@ -6,12 +6,64 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Twilio\Jwt\AccessToken;
 use Twilio\Jwt\Grants\ChatGrant;
+use Twilio\Jwt\Grants\VideoGrant;
+use Illuminate\Support\Facades\Validator;
+
 
 class TwilioController extends Controller
 {
+    
+    public function generate_token(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'identity'=>'required',
+            'room_name'=>'required'
+          ]);
+  
+          if ($validator->fails()) {
+              $errorMessage = implode(',', $validator->errors()->all());
+              return response()->json(['errors' => $errorMessage], 422);
+          }
+        $accountSid = 'ACc854bb7e38224443be49cbb9e321856f';
+        $apiKeySid = 'SK718fb7ab87c330a9929d7b5295974c0e';
+        $apiKeySecret = 'L0QF7nOA6jTiVpJYoUvxUJhG82Xyitli';
+        /*
+        $accountSid = ('TWILIO_ACCOUNT_SID');
+        $apiKeySid = ('TWILIO_API_KEY');
+        $apiKeySecret = ('TWILIO_API_SECRET');
+        */
+        $identity = $request->identity;
+        $room_name = $request->room_name;
+
+         // Create an Access Token
+        $token = new AccessToken(
+        $accountSid,
+        $apiKeySid,
+        $apiKeySecret,
+        3600,
+        $identity,
+        $room_name
+        );
+
+        // Grant access to Video
+        $grant = new VideoGrant();
+        $grant->setRoom($room_name);
+        $token->addGrant($grant);
+
+        // Serialize the token as a JWT
+        /*
+        $result=[ 
+            "identity" => $identity,
+            "token"=> $token->toJWT()
+        ];
+        return response()->json($result);
+        */
+        $response = ["identity"=>$identity,"token"=>$token->toJWT()];
+        return response()->json($response, 200);
+        
+    }
     public function ChatCreateTokens(Request $request)
     {
-        dd(auth()->user());
         // Required for all Twilio access tokens
         $twilioAccountSid = 'ACc854bb7e38224443be49cbb9e321856f';
         $twilioApiKey = 'SK718fb7ab87c330a9929d7b5295974c0e';
